044baacf44b59968ef18bc0e4089ea95a207edaa
GLNRO <lg@glnro.com>
2015-11-06 00:24:03 -0500

    Define prime? method

---
+++ project_euler/problem_3/problem_3.rb
@@ -0,0 +1,33 @@
+# prime factorization of 600851475143
+# half is 300425737571
+# will be smaller than 775146
+#
+
+def next_prime(num)
+
+end
+
+def prime?(num)
+  p = true
+  (2..num/2).each {|x| p = false if num % x == 0}
+  p
+end
+
+def largest_factor(n)
+  factors = []
+  f = 2
+  #until n = 1
+    #if n % f == 0
+      # push f to factors
+      # divide n by f
+    #else
+      # f+=1
+      # prime?(f)
+
+
+
+end
+
+
+puts (600851475143/2)
+puts (Math.sqrt(600851475143.00))
