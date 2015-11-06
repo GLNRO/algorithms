2b359e486c9643f33bfda8ce379cc13b9585f5f1
GLNRO <lg@glnro.com>
2015-11-06 00:54:08 -0500

    Complete first solution for problem 3

--- project_euler/problem_3/problem_3.rb
+++ project_euler/problem_3/problem_3.rb
@@ -1,10 +1,9 @@
-# prime factorization of 600851475143
-# half is 300425737571
-# will be smaller than 775146
-#
-
 def next_prime(num)
-
+  f = num +1
+  until prime?(f)
+    f+=1
+  end
+  f
 end
 
 def prime?(num)
@@ -16,18 +15,15 @@ end
 def largest_factor(n)
   factors = []
   f = 2
-  #until n = 1
-    #if n % f == 0
-      # push f to factors
-      # divide n by f
-    #else
-      # f+=1
-      # prime?(f)
-
-
-
+  until n == 1
+    if n % f == 0
+      factors << f
+      n = n/f
+    else
+      f = next_prime(f)
+    end
+  end
+  p factors.last
 end
 
-
-puts (600851475143/2)
-puts (Math.sqrt(600851475143.00))
+largest_factor(600851475143)
